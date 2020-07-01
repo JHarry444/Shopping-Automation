f59fd7086ddba946151965e25e3c18be22f687b0
Jordan <jordan.harrison@qa.com>
2020-07-01 12:54:17 +0100

    cfg

--- src/test/java/com/qa/shoppingAuto/tests/TestPurchase.java
+++ src/test/java/com/qa/shoppingAuto/tests/TestPurchase.java
@@ -1,11 +1,10 @@
 package com.qa.shoppingAuto.tests;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
 import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
@@ -18,20 +17,16 @@ import com.qa.shoppingAuto.pages.Cart;
 import com.qa.shoppingAuto.pages.Home;
 import com.qa.shoppingAuto.pages.Item;
 import com.qa.shoppingAuto.pages.Search;
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
 
 public class TestPurchase {
 
 	private WebDriver driver;
-	private static ExtentReports extent = new ExtentReports("report.html", true);
 
 	@Before
 	public void setup() {
 		System.setProperty(Constants.PROPERTY, Constants.PATH);
 		ChromeOptions options = new ChromeOptions();
-		options.setHeadless(true);
+		options.setHeadless(false);
 		driver = new ChromeDriver(options);
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
@@ -39,13 +34,16 @@ public class TestPurchase {
 	@Test
 	public void test() {
 		driver.get("http://automationpractice.com/index.php");
-		ExtentTest test  = extent.startTest("Purchase test");
+
 		Home homePage = PageFactory.initElements(driver, Home.class);
 		homePage.search("Blouse");
+
 		Search searchPage = PageFactory.initElements(driver, Search.class);
 		searchPage.selectItem();
+
 		Item itemPage = PageFactory.initElements(driver, Item.class);
 		itemPage.addToCart();
+
 		Cart cartPage = PageFactory.initElements(driver, Cart.class);
 		cartPage.proceed();
 		cartPage.login("a@b2.com", "password");
@@ -53,23 +51,13 @@ public class TestPurchase {
 		cartPage.proceedToPayment();
 		cartPage.pay();
 		cartPage.confirm();
-		if ("Your order on My Store is complete.".equals(cartPage.getConfirmation())) {
-			test.log(LogStatus.PASS, "Yay");
-		} else {
-			test.log(LogStatus.FAIL,"Purchase failed");
-			extent.endTest(test);
-			fail("Purchase failed");
-		}
+
+		assertEquals("Your order on My Store is complete.", cartPage.getConfirmation());
 	}
 
 	@After
 	public void tearDown() {
 		this.driver.close();
-	}
-	
-	@AfterClass
-	public static void flushReport() {
-		extent.flush();
 	}
 
 }
